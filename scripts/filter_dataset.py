88623c23663b1dae78a7576dda0e99c724d611bd
Anton Gladyr <anton.gladyr@gmail.com>
2020-12-04 20:52:17 -0500

    minor fix

--- scripts/filter_dataset.py
+++ scripts/filter_dataset.py
@@ -25,33 +25,20 @@ def clean_data(posts_dataset, keywords) -> np.array:
     regex_keywords = ''
     for keyword in keywords:
         if keyword == keywords[0]:
-            regex_keywords += f'{keyword}|{keyword.upper()}'
+            regex_keywords += fr'\b\W*{keyword}\W*\b|\b\W*{keyword.upper()}\W*\b'
         else:
-            regex_keywords += f'|{keyword}|{keyword.upper()}'
+            regex_keywords += fr'|\b\W*{keyword}\W*\b|\b\W*{keyword.upper()}\W*\b'
 
-    #regexp = re.compile(fr'\b\W*{regex_keywords}\W*\b')
-
-    regexp = fr'\b\W*{regex_keywords}\W*\b'
-    #rows_to_keep = [bool(regexp.search(title)) for val in posts_dataset[:, POST_TITLE_INDEX]]
-    #posts_dataset = posts_dataset[rows_to_keep]
+    regexp = regex_keywords
     posts_dataset = posts_dataset[posts_dataset['title'].str.contains(regexp, case=True, regex=True)]
     return posts_dataset
-
-
-        #tweets_dataset = tweets_dataset[tweets_dataset[:, TWEET_LANGUAGE_INDEX] == ENGLISH_LANG]
-
-        #trump_mention_tweets = np.array([bool(re.search(r'\b\W*Trump\W*\b', val)) for val in tweets_dataset[:, TWEET_CONTENT_INDEX]])
-        #trump_mention_col = np.array(np.where(trump_mention_tweets == True, 'T', 'F'))
-        #trump_mention_col = np.reshape(trump_mention_col, (trump_mention_col.shape[0], 1))
-   
-    #return np.array(np.hstack((posts_dataset, trump_mention_col)))
 
 
 def load_posts_df(dataset_path: str) -> pd.DataFrame: 
     # check if the given dataset file exists
     posts_file_exists = osp.isfile(dataset_path)
     if posts_file_exists == False:
-        print(f'File \'{args.input}\' does not exist')
+        print(f'File \'{dataset_path}\' does not exist')
         exit()
     
     return pd.read_csv(dataset_path, delimiter='\t', skipinitialspace=True)
